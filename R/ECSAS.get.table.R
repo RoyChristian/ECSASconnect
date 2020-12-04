440bc0d3d7fb7a6e424f28b2449440443360decf
Dave Fifield <dave.fifield@cananda.ca>
2020-12-04 15:32:08 -0330

    - channel name typo and param reorder

--- R/ECSAS.get.table.R
+++ R/ECSAS.get.table.R
@@ -4,13 +4,12 @@
 #'
 #'@description This function will connect to the Access database and return a table as a data frame.
 #'
-#'@param ecsas.path (default NULL) full path name to the ECSAS database. 
 #'@param table character vector (of length 1) naming table to be returned.
+#'@param ecsas.path (default NULL) full path name to the ECSAS database. 
 #' 
 #'@section Author:Dave Fifield
 #'
-
-ECSAS.get.table <-  function(ecsas.path = NULL, table = NULL, debug = FALSE) {
+ECSAS.get.table <- function(table = NULL, ecsas.path = NULL, debug = FALSE) {
 
   coll = checkmate::makeAssertCollection()
   checkmate::assert_file_exists(ecsas.path, add = coll)
@@ -30,7 +29,7 @@ ECSAS.get.table <-  function(ecsas.path = NULL, table = NULL, debug = FALSE) {
   channel1 <- RODBC::odbcConnectAccess(ecsas.path, uid="") 
   
   ret <- RODBC::sqlFetch(channel1, table) %>% ensure_data_is_returned 
-  RODBC::odbcClose(channecl1)
+  RODBC::odbcClose(channel1)
   
   ret
 }
